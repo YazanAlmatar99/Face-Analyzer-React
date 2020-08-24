9c2d90cbfdd0a01b7355d505841aa80b66164727
Yazan <yazanmatar90@gmail.com>
2020-08-23 21:05:33 -0400

    powered by text

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "./App.css";
 import Upload from './Components/Upload'
-import axios from 'axios';
 function App() {
  
   return (
     <div>
       <Upload/>
+      <h5 className="powered-text">Powered by <a href="http://www.yazanalmatar.com" target="_blank">Yazan Almatar </a> &copy; and <a href="https://azure.microsoft.com/en-us/services/cognitive-services/face/">Microsoft Azure</a> &trade;</h5>
     </div>
   );
 }
